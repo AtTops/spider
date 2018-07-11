6f5d9f8facf7b66107d209a055c0664db43a2095
wanghai <wang6hai@gmail.com>
2018-07-11 17:46:48 +0800

    图片下载成功

---
+++ src/test/java/spider/mglp/service/impl/DownloadImgsImplTest.java
@@ -0,0 +1,36 @@
+package spider.mglp.service.impl; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* DownloadImgsImpl Tester. 
+* 
+* @author <wang> 
+* @since <pre>07/11/2018</pre> 
+* @version 1.0 
+*/ 
+public class DownloadImgsImplTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: downloadImgAndNamed() 
+* 
+*/ 
+@Test
+public void testDownloadImgAndNamed() throws Exception { 
+    DownloadImgsImpl downloadImgs = new DownloadImgsImpl();
+    downloadImgs.downloadImgAndNamed();
+} 
+
+
+} 
