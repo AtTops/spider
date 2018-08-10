59760711c5c05b6f428373ba7dd2bbc3dd59d462
wanghai <wang6hai@gmail.com>
2018-08-10 17:04:56 +0800

    移动到spider仓库

---
+++ src/main/java/spider/mglp/service/impl/SpuEveryday.java
@@ -0,0 +1,13 @@
+package spider.mglp.service.impl;
+
+/**
+ * <p>pakage: spider.mglp.service.impl</p>
+ * <p>
+ * descirption:
+ *
+ * @author wanghai
+ * @version V1.0
+ * @since <pre>2018/8/10 下午2:28</pre>
+ */
+public class SpuEveryday {
+}
